746a22e9fb612568620779ecd7f66cab2ad7ddbb
David Fauthoux <david.fauthoux@gmail.com>
2021-05-17 17:19:03 +0300

    Not generating image anymore

--- api-generate.js
+++ api-generate.js
@@ -6,11 +6,11 @@ Plugins.generate = function(params, language) {
 			let div = $("<div>");
 			let divs = $("<div>").addClass("buttons");
 
-			let percentDiv = $("<div>").addClass("percent");
+			// let percentDiv = $("<div>").addClass("percent");
 			let rendering = false;
 
 			let renderIt = function() {
-				percentDiv.text(0 + "%");
+				// percentDiv.text(0 + "%");
 
 				let render = $("<div>").addClass("render");
 
@@ -53,44 +53,48 @@ Plugins.generate = function(params, language) {
 					}
 				}
 
-				let cut = $("<div>");
-				cut.css("position", "absolute");
-				cut.css("top", "0");
-				cut.css("left", "0");
-				cut.css("overflow", "scroll");
-				cut.css("width", "0");
-				cut.css("height", "0");
-				cut.append(render);
-				$("body").prepend(cut);
+				var w = window.open("", '_blank');
+				w.document.write("<html><head><style>body { width: 500px; margin: 0 auto; font-size: 14px; } img { display: block; margin: 30px auto; width: 300px; } .block { margin-bottom: 5px; text-align: justify; } .frame { width: 300px; margin: 10px auto; border: dotted 1px gray; height: 150px; padding: 5px; background: #eee; color: gray; } </style></head><body>" + render.html() + "<script>window.print();</script></body></html>");
+				w.document.close(); 
 
-				window.scrollTo(0, 0);
-				html2canvas(render[0]).then(function(canvas) { // Will not be updated according to language if flag clicked
-					cut.remove();
+				// let cut = $("<div>");
+				// cut.css("position", "absolute");
+				// cut.css("top", "0");
+				// cut.css("left", "0");
+				// cut.css("overflow", "scroll");
+				// cut.css("width", "0");
+				// cut.css("height", "0");
+				// cut.append(render);
+				// $("body").prepend(cut);
 
-					let uploadDirectory = "upload";
-					let imageType = "jpeg";
-					let jpegQuality = 0.9;
+				// window.scrollTo(0, 0);
+				// html2canvas(render[0]).then(function(canvas) { // Will not be updated according to language if flag clicked
+				// 	cut.remove();
+
+				// 	let uploadDirectory = "upload";
+				// 	let imageType = "jpeg";
+				// 	let jpegQuality = 0.9;
 				
-					let nameHeap = new Heap("generated.jpg");
-					let outputBlobHeap = new Heap();
-					let outputPathHeap = new Heap();
+				// 	let nameHeap = new Heap("generated.jpg");
+				// 	let outputBlobHeap = new Heap();
+				// 	let outputPathHeap = new Heap();
 				
-					try_(sequence_(
-						ImageUtils.canvasToBlob(new Heap(canvas), new Heap(imageType), new Heap(jpegQuality), outputBlobHeap),
-						UploadUtils.uploadBlob(server, new Heap(uploadDirectory), outputBlobHeap, nameHeap, {
-							set: function(p) {
-								percentDiv.text(Math.round(p * 100) + "%");
-							}
-						}, outputPathHeap)))
-					.res(function() {
-						rendering = false;
-						printButton.off("click");
-						printButton.attr("download", "").attr("href", server.base + "/" + outputPathHeap.get()).attr("target", "_blank");
-						setTimeout(function() {
-							printButton[0].click();
-						}, 0);
-					}).run();
-				});
+				// 	try_(sequence_(
+				// 		ImageUtils.canvasToBlob(new Heap(canvas), new Heap(imageType), new Heap(jpegQuality), outputBlobHeap),
+				// 		UploadUtils.uploadBlob(server, new Heap(uploadDirectory), outputBlobHeap, nameHeap, {
+				// 			set: function(p) {
+				// 				percentDiv.text(Math.round(p * 100) + "%");
+				// 			}
+				// 		}, outputPathHeap)))
+				// 	.res(function() {
+				// 		rendering = false;
+				// 		printButton.off("click");
+				// 		printButton.attr("download", "").attr("href", server.base + "/" + outputPathHeap.get()).attr("target", "_blank");
+				// 		setTimeout(function() {
+				// 			printButton[0].click();
+				// 		}, 0);
+				// 	}).run();
+				// });
 			};
 
 			let printButton = $("<a>").addClass("button"); // .addClass("secondary");
@@ -115,7 +119,7 @@ Plugins.generate = function(params, language) {
 				callback("");
 			});
 
-			div.append(percentDiv);
+			// div.append(percentDiv);
 			div.append(divs);
 			return div;
 		},
