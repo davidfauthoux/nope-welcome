accf2aa6d0662a7fe95ac318095660f0863f0d62
David Fauthoux <david.fauthoux@gmail.com>
2022-09-03 00:42:16 +0300

    wwip

--- api-signature.js
+++ api-signature.js
@@ -58,7 +58,7 @@ export default {
 										t = replaceIn(t, k, allData[k].value);
 									}
 								}
-								t = replaceIn(t, "today", ((data === undefined) || (data.date === undefined)) ? i18n.today() : data.date);
+								t = replaceIn(t, "today", ((data === undefined) || (data.date === undefined)) ? "" : data.date);
 
 								let replaceUnknown = function(tt, to) {
 									let r = {};
